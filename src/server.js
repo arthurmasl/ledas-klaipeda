640b9ecf4e895213a14fa12d18d0a93b9de3c8c2
Artur Maslobojev <arthur.masloboev@updateport.com>
2019-05-29 02:35:13 +0300

    serv

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(cors());
